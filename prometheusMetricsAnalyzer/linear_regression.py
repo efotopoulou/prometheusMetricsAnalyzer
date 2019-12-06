f92a42740d43c5b8f91218a34d76a33d17e3477e
efotopoulou <fwtopoulou@gmail.com>
2019-12-06 17:20:58 +0200

    update test function

--- prometheusMetricsAnalyzer/linear_regression.py
+++ prometheusMetricsAnalyzer/linear_regression.py
@@ -8,8 +8,7 @@ def ln(prometheus_url,periods,metrics,step):
 
     message = """<html>
     <head></head>
-    <body><p>This is a test analytics results page comming from prometheusMetricsAnalyzer python package</p>
-    <img src="/static/plot.png" alt="Smiley face" height="200" width="200"></body>
+    <body><p>This is a test analytics results page comming from prometheusMetricsAnalyzer python package</p></body>
     </html>"""
 
     return message
