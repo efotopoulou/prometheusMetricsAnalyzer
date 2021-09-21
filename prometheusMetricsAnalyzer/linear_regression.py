9ae68af548520126a80408bd3b22c63ee82f7d55
efotopoulou <fwtopoulou@gmail.com>
2021-09-21 13:46:55 +0300

    Update linear_regression.py

--- prometheusMetricsAnalyzer/linear_regression.py
+++ prometheusMetricsAnalyzer/linear_regression.py
@@ -4,8 +4,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def test(prometheus_url,periods,metrics,step):
+
+    message = """<html>
+    <head></head>
+    <body><p>This is a test analytics results page comming from prometheusMetricsAnalyzer python package</p></body>
+    </html>"""
+
+    return message
+
 def ln(prometheus_url,periods,metrics,step):
 
+    print('geia sou')
     message = """<html>
     <head></head>
     <body><p>This is a test analytics results page comming from prometheusMetricsAnalyzer python package</p></body>
