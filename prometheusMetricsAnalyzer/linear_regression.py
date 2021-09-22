353b467751f827722f130c3f26598cad33207ad9
efotopoulou <efotopoulou@netmode.ntua.gr>
2021-09-22 18:02:47 +0300

    update kmeans

--- prometheusMetricsAnalyzer/linear_regression.py
+++ prometheusMetricsAnalyzer/linear_regression.py
@@ -14,32 +14,3 @@ def ln(prometheus_url,periods,metrics,step):
     </html>"""
 
     return message
-
-def kmeans(prometheus_url,periods,metrics,step):
-
-    print('geia sou')
-    endpoint = prometheus_url + '/api/v1/query_range'
-    for metric in metrics:
-        response =requests.get(endpoint, params={'query': metric,'start':'2021-09-19T07:00:00.781Z','end':'2021-09-19T13:00:00.781Z','step':'3m'})
-    #response.content
-    X = np.array([[1, 2], [1, 4], [1, 0], [10, 2], [10, 4], [10, 0]])
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
-
-    #Getting unique labels
-    label = kmeans.labels_
-    u_labels = np.unique(label)
-
-    #plotting the results:
-    for i in u_labels:
-        plt.scatter(X[label == i , 0] , X[label == i , 1] , label = i)
-    plt.legend()
-    #plt.show()
-    #plt.savefig('templates/foo.png')
-    message = """<html>
-    <head></head>
-    <body><p>This is a linear regression analytics results page comming from prometheusMetricsAnalyzer python package</p>
-    <img src="plot.png" alt="foo" width="500" height="600">
-    </body>
-    </html>"""
-
-    return message,plt
