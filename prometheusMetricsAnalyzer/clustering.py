e409041f7f3ca6f97526d1861abb80c966fbefd0
efotopoulou <efotopoulou@netmode.ntua.gr>
2021-09-27 14:39:15 +0300

    consume prometheus metrics

--- prometheusMetricsAnalyzer/clustering.py
+++ prometheusMetricsAnalyzer/clustering.py
@@ -4,16 +4,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import requests
 from sklearn.cluster import KMeans
-
+import json
 
 def kmeans(prometheus_url,periods,metrics,step):
 
+    #periods":[{"start":"2021-09-19T07:00:00.781Z","end":"2021-09-19T13:00:00.781Z"}]
+    periods = periods[0]
+    print(periods['start'])
     endpoint = prometheus_url + '/api/v1/query_range'
+    dataset = np.empty((0))
     for metric in metrics:
-        response =requests.get(endpoint, params={'query': metric,'start':'2021-09-19T07:00:00.781Z','end':'2021-09-19T13:00:00.781Z','step':'3m'})
-    #response.content
-    X = np.array([[1, 2], [1, 4], [1, 0], [10, 2], [10, 4], [10, 0]])
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
+        response =requests.get(endpoint, params={'query': metric,'start':periods['start'],'end':periods['end'],'step':step})
+        y = json.loads(response.content)
+        values = y["data"]["result"][0]["values"]
+        np_values = np.array(values)
+        np_values = np.delete(np_values,0,1)
+        if len(dataset)==0:
+            dataset = np_values
+        else:
+            dataset = np.hstack((dataset, np_values))
+
+        print(dataset)
+
+    #X = np.array([[1, 2], [1, 4], [1, 0], [10, 2], [10, 4], [10, 0]])
+    kmeans = KMeans(n_clusters=2, random_state=0).fit(dataset)
 
     #Getting unique labels
     label = kmeans.labels_
@@ -21,8 +35,8 @@ def kmeans(prometheus_url,periods,metrics,step):
 
     #plotting the results:
     for i in u_labels:
-        plt.scatter(X[label == i , 0] , X[label == i , 1] , label = i)
-    plt.legend()
+        plt.scatter(dataset[label == i , 0] , dataset[label == i , 1] , label = i)
+    #plt.legend()
     #plt.show()
     #plt.savefig('templates/foo.png')
     message = """<html>
